048bf822571e1bc1a7801d875444ab95b18acb45
CHAVAN <CHAVAN@KALYAN>
2022-03-31 14:35:44 +0530

    this is kailas

---
+++ com.crm.kingstar/com/crm/autodesk/indigoflights/CreateIndigo.java
@@ -0,0 +1,17 @@
+package com.crm.autodesk.indigoflights;
+
+import org.testng.annotations.Test;
+
+public class CreateIndigo {
+	@Test
+	public void createIndigoflights()
+	{
+
+		System.out.println("welcome to spicejet....");
+
+		System.out.println("flights data has been "); 
+		System.out.println("hello kailash");
+
+	}
+
+}
